0ad5064ca9a683e41afa57096f8c421e2a9baafa
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-05-24 23:52:41 +0200

    compile javascript from typescript code

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
+"use strict";
+// Define the income variable
 let income = 0;
+// Define the expenses array
 let expenses = [];
-
-document.getElementById('add-income').addEventListener('click', function() {
+// Add an event listener to the add-income button
+document.getElementById('add-income').addEventListener('click', function () {
     income = Number(document.getElementById('income').value);
     updateIncomeInSummary();
     updateRemainderInSummary();
 });
-
-document.getElementById('add-expense').addEventListener('click', function() {
-  const expenseAmount = Number(document.getElementById('expense-amount').value);
-
-  let totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
-  if (totalExpenses + expenseAmount > income) {
-      alert("This expenditure will exceed your income. Please review your expenses.");
-      return;
-  }
-
-  const expenseCategory = document.getElementById('expense-category').value;
-  expenses.push({category: expenseCategory, amount: expenseAmount});
-  updateExpensesInSummary();
-  updateRemainderInSummary();
+// Add an event listener to the add-expense button
+document.getElementById('add-expense').addEventListener('click', function () {
+    const expenseAmount = Number(document.getElementById('expense-amount').value);
+    let totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
+    if (totalExpenses + expenseAmount > income) {
+        alert("This expenditure will exceed your income. Please review your expenses.");
+        return;
+    }
+    const expenseCategory = document.getElementById('expense-category').value;
+    expenses.push({ category: expenseCategory, amount: expenseAmount });
+    updateExpensesInSummary();
+    updateRemainderInSummary();
 });
-
-
+// Function to update the income in the summary
 function updateIncomeInSummary() {
     const table = document.getElementById('summary-table');
     const row = table.insertRow(0);
@@ -31,34 +31,31 @@ function updateIncomeInSummary() {
     cell1.innerHTML = "Income";
     cell2.innerHTML = "$" + income;
 }
-
+// Function to update the expenses in the summary
 function updateExpensesInSummary() {
     const table = document.getElementById('summary-table');
-    const row = table.insertRow(-1); // Inserts a row at the end of the table
+    const row = table.insertRow(-1);
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
     cell1.innerHTML = expenses[expenses.length - 1].category;
     cell2.innerHTML = "$" + expenses[expenses.length - 1].amount;
-
     const remainderRow = document.getElementById('remainder-row');
     if (remainderRow) {
-        // Move the "Remainder" row to the end of the table
         table.appendChild(remainderRow);
     }
 }
-
+// Function to update the remainder in the summary
 function updateRemainderInSummary() {
-    // Calculate total expenses and remaining budget
     let totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
     let remainingBudget = income - totalExpenses;
-
-    // Update remainder row if it exists, otherwise create a new one
     const table = document.getElementById('summary-table');
     let row = document.getElementById('remainder-row');
     if (row) {
         row.cells[1].innerHTML = "$" + remainingBudget;
-    } else {
-        row = table.insertRow(-1);
+    }
+    else {
+        const table = document.getElementById('summary-table');
+        const row = table.insertRow(-1);
         const cell1 = row.insertCell(0);
         const cell2 = row.insertCell(1);
         cell1.innerHTML = "Remainder";
@@ -66,25 +63,23 @@ function updateRemainderInSummary() {
         row.id = "remainder-row";
     }
 }
-
+// Function to add an expense
 function addExpense() {
-  var category = document.getElementById('expense-category').value;
-  var amount = document.getElementById('expense-amount').value;
-
-  if (amount > remainder) {
-    alert("This expenditure will exceed your income. Please review your expenses.");
-    return;
-  }
-  
-  var row = document.createElement('tr');
-  var categoryCell = document.createElement('td');
-  var amountCell = document.createElement('td');
-  categoryCell.textContent = category;
-  amountCell.textContent = "$" + amount;
-  row.appendChild(categoryCell);
-  row.appendChild(amountCell);
-  document.getElementById('summary-table').appendChild(row);
-
-  remainder -= amount;
-  updateRemainder();
+    const category = document.getElementById('expense-category').value;
+    const amount = Number(document.getElementById('expense-amount').value);
+    let totalExpenses = expenses.reduce((total, expense) => total + expense.amount, 0);
+    const remainder = income - totalExpenses;
+    if (amount > remainder) {
+        alert("This expenditure will exceed your income. Please review your expenses.");
+        return;
+    }
+    const row = document.createElement('tr');
+    const categoryCell = document.createElement('td');
+    const amountCell = document.createElement('td');
+    categoryCell.textContent = category;
+    amountCell.textContent = "$" + amount;
+    row.appendChild(categoryCell);
+    row.appendChild(amountCell);
+    document.getElementById('summary-table').appendChild(row);
+    updateRemainderInSummary();
 }
